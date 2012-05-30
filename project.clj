3fb5f883c8f965a0e602550ea6a32e6c1263472a
Ron Toland <rontoland@gmail.com>
2012-05-30 06:41:49 -0700

    Add leiningen project files

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject stylo "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/stylo/test/core.clj
@@ -0,0 +1,6 @@
+(ns stylo.test.core
+  (:use [stylo.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
