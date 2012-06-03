1e883987c34fc708464d78d8a442e3cd7810a12b
Ron Toland <rontoland@gmail.com>
2012-06-03 11:11:35 -0700

    Initial code to try reversing a string from the queue and sending it back

--- src/stylo/core.clj
+++ src/stylo/core.clj
@@ -1,22 +1,31 @@
 (ns stylo.core
   (:use com.mefesto.wabbitmq))
 
-(use 'com.mefesto.wabbitmq)
+(def test-broker {:host "localhost" :username "guest" :password "guest"})
 
-(with-broker {:host "localhost" :username "guest" :password "guest"}
+(with-broker test-broker 
   (with-channel
     (exchange-declare "test.exchange" "direct")
     (queue-declare "test.queue")
     (queue-bind "test.queue" "test.exchange" "test")))
 
-(with-broker {:host "localhost" :username "guest" :password "guest"}
+(with-broker test-broker 
   (with-channel
-    (with-exchange "test.exchange"
-      (publish "test" (.getBytes "Hello world!")))))
+    (exchange-declare "test-response.exchange" "direct")
+    (queue-declare "test-response.queue")
+    (queue-bind "test-response.queue" "test-response.exchange" "test-response")))
 
-(defn consumer []
-  (with-broker {:host "localhost" :username "guest" :password "guest"}
+(defn test-consumer []
+  (with-broker test-broker 
     (with-channel
       (with-queue "test.queue"
         (doseq [msg (consuming-seq true)] ; consumes messages with auto-acknowledge enabled
-          (println "received:" (String. (:body msg))))))))
+          (send-reverse (String. (:body msg)))
+
+(defn send-reverse
+  "Reverse the incoming string and send it back to the message queue"
+  [original]
+  (with-broker test-broker
+    (with-channel
+      (with-exchange "test-response.exchange"
+        (publish "test-response" (.getBytes (apply str (reverse original))))))))
