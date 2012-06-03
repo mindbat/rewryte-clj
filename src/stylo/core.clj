51916eca1f9f614d446090fb67c1f0756cbaec32
Ron Toland <rontoland@gmail.com>
2012-06-03 11:00:37 -0700

    Initial test code for rabbitmq communication

--- src/stylo/core.clj
+++ src/stylo/core.clj
@@ -1,1 +1,22 @@
-(ns stylo.core)
+(ns stylo.core
+  (:use com.mefesto.wabbitmq))
+
+(use 'com.mefesto.wabbitmq)
+
+(with-broker {:host "localhost" :username "guest" :password "guest"}
+  (with-channel
+    (exchange-declare "test.exchange" "direct")
+    (queue-declare "test.queue")
+    (queue-bind "test.queue" "test.exchange" "test")))
+
+(with-broker {:host "localhost" :username "guest" :password "guest"}
+  (with-channel
+    (with-exchange "test.exchange"
+      (publish "test" (.getBytes "Hello world!")))))
+
+(defn consumer []
+  (with-broker {:host "localhost" :username "guest" :password "guest"}
+    (with-channel
+      (with-queue "test.queue"
+        (doseq [msg (consuming-seq true)] ; consumes messages with auto-acknowledge enabled
+          (println "received:" (String. (:body msg))))))))
