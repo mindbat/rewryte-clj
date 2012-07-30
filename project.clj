fb32d16e44ce89d86505ec7a702cc75438684e0e
Ron Toland <rontoland@gmail.com>
2012-07-30 07:48:12 -0700

    Remove mongoika dependency. Add monger dependency.

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Application for calculating writing style"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [com.mefesto/wabbitmq "0.2.1"]
-                 [mongoika "0.6.12"]]
+                 [com.novemberain/monger "1.1.2"]]
   :main rewryte.core
   :uberjar-name "rewryte.jar")
