1e6e05119bb1bb4fe8802363cf0bcaf89b4fd9d3
Ron Toland <rontoland@gmail.com>
2012-07-31 07:26:15 -0700

    Add initial functions to save and load docs from mongo

--- src/rewryte/mongo.clj
+++ src/rewryte/mongo.clj
@@ -1,13 +1,22 @@
 (ns rewryte.mongo
-  (:use mongoika))
+  (:require [monger.core :as mcore] 
+            [monger.collection :as mcoll]))
+
+(def mongo-host {:host "127.0.0.1" :port 27017})
 
 (defn save-results
   "Save the given results to mongodb"
   [user-id doc-name results]
-  (with-mongo [connection {:host "127.0.0.1" :port 27107}]
-    (with-db-binding (database connection :data)
-      (update! :$set {:results results} (restrict :user_id user-id :document_name doc-name :docs)))))
+  (let [doc-match {:user_id user-id :document_name doc-name}
+        doc-update {:results results}]
+    (mcore/connect! mongo-host)
+    (mcore/set-db! (mcore/get-db "data"))
+    (mcoll/update "docs" doc-match {:$set doc-update})))
 
 (defn get-document
   "Fetch the given document from mongodb"
-  [user-id doc-name])
+  [user-id doc-name]
+  (let [doc-match {:user_id user-id :document_name doc-name}]
+    (mcore/connect! mongo-host)
+    (mcore/set-db! (mcore/get-db "data"))
+    (mcoll/find-maps "docs" doc-match))) 
