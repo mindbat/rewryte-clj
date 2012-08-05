566d9452a4c19b27f770f9a2eae50a52780a9b70
Ron Toland <rontoland@gmail.com>
2012-08-05 16:17:33 -0700

    Break out rabbit-mq functions into separate library

--- src/rewryte/core.clj
+++ src/rewryte/core.clj
@@ -1,29 +1,12 @@
 (ns rewryte.core
   (:gen-class :main true)
-  (:use com.mefesto.wabbitmq, rewryte.process, rewryte.mongo, clojure.string, cheshire.core))
+  (:use rewryte.rabbit, rewryte.process, rewryte.mongo, clojure.string, cheshire.core))
 
 (def rewryte-broker {:host "localhost" :username "guest" :password "guest"})
 
-(with-broker rewryte-broker 
-  (with-channel
-    (exchange-declare "general-response.exchange" "direct")
-    (queue-declare "general-response.queue")
-    (queue-bind "general-response.queue" "general-response.exchange" "general-response")))
+(declare-queue rewyte-broker "general-response.exchange" "general-response.queue" "general-response")
 
-(with-broker rewryte-broker 
-  (with-channel
-    (exchange-declare "frequency.exchange" "direct")
-    (queue-declare "frequency.queue")
-    (queue-bind "frequency.queue" "frequency.exchange" "frequency")))
-
-(defn declare-queue
-  "Declare a new rabbit-mq queue"
-  [exchange-name queue-name short-name]
-  (with-broker rewryte-broker
-    (with-channel
-      (exchange-declare exchange-name "direct")
-      (queue-declare queue-name)
-      (queue-bind queue-name exchange-name short-name))))
+(declare-queue rewyte-broker "frequency.exchange" "frequency.queue" "frequency")
 
 (defn send-results-published
   "Notify rabbitmq that the user results are ready"
@@ -31,28 +14,20 @@
   (let [exchange-name (str user-id "-response.exchange")
         queue-name (str user-id "-response.queue")
         short-name (str user-id "-response")]
-    (declare-queue exchange-name queue-name short-name)
-    (with-broker rewryte-broker
-      (with-channel
-        (with-exchange exchange-name 
-          (publish short-name (.getBytes doc-name)))))))
+    (declare-queue rewryte-broker exchange-name queue-name short-name)
+    (rabbit-publish rewryte-broker exchange-name short-name doc-name)))
 
-(defn freq-consumer []
-  (with-broker rewryte-broker
-    (with-channel
-      (with-queue "frequency.queue"
-        (doseq [msg (consuming-seq true)]
-          (let [body (String. (:body msg))
-                split-body (split body #":")
-                user-id (Integer/parseInt (first split-body))
-                doc-name (second split-body)
-                mongo-doc (get-document user-id doc-name)
-                document (mongo-doc :document)
-                results (generate-string (count-words document))]
-            (save-results user-id doc-name results)
-            (send-results-published user-id doc-name)))))))
+(defn frequency-consumer [message-body]
+  (let [split-body (split body #":")
+        user-id (Integer/parseInt (first split-body))
+        doc-name (second split-body)
+        mongo-doc (get-document user-id doc-name)
+        document (mongo-doc :document)
+        results (generate-string (count-words document))]
+    (save-results user-id doc-name results)
+    (send-results-published user-id doc-name)))
 
 (defn -main [consumer]
   (cond
-    (= consumer "frequency") (freq-consumer)
+    (= consumer "frequency") (start-consumer rewryte-broker "frequency.queue" frequency-consumer)
     :else (println "No consumer by that name available")))
---
+++ src/rewryte/rabbit.clj
@@ -0,0 +1,28 @@
+(ns rewryte.rabbit
+  (:use com.mefesto.wabbitmq))
+
+(defn declare-queue
+  "Declare a new rabbit-mq queue"
+  [broker-map exchange-name queue-name short-name]
+  (with-broker broker-map 
+    (with-channel
+      (exchange-declare exchange-name "direct")
+      (queue-declare queue-name)
+      (queue-bind queue-name exchange-name short-name))))
+
+(defn start-consumer
+  "Start up a consumer for a given queue"
+  [broker-map queue-name consumer-function]
+  (with-broker broker-name
+    (with-channel
+      (with-queue queue-name
+        (doseq [msg (consuming-seq true)]
+          (consumer-function (String. (:body msg))))))))
+
+(defn rabbit-publish
+  "Publish the given content to a rabbit-mq queue"
+  [broker-map exchange-name short-name content]
+  (with-broker broker-map
+    (with-channel
+      (with-exchange exchange-name
+        (publish short-name (.getBytes content))))))
