c53c1954de430332e10a8036bba0f3c905a4db70
Ron Toland <rontoland@gmail.com>
2012-08-19 13:32:56 -0700

    Add functions for paginating text according to the number of words on each page

--- src/rewryte/process.clj
+++ src/rewryte/process.clj
@@ -1,6 +1,19 @@
 (ns rewryte.process
   (:use clojure.string))
 
+(defn split-n
+  "Split the given sequence into a vector of vectors made of every nth element of the original sequence.
+   Ex:
+      (split-n 2 [1 3 5 7 9 11 13 15 17 19 21])
+    => [[1 3] [5 7] [9 11] [13 15] [17 19] [21]]"
+  [n split-seq]
+  (loop [take-num n
+         rtn-seq []
+         process-seq split-seq]
+    (if (< (count process-seq) take-num)
+      (conj rtn-seq (vec process-seq))
+      (recur take-num (conj rtn-seq (vec (take n process-seq))) (drop n process-seq)))))
+
 (defn convert-to-words
   "Convert a line into a sequence of words"
   [line]
@@ -15,3 +28,8 @@
   "Count the number of words in a string"
   [incoming]
   (frequencies (convert-to-keywords incoming)))
+
+(defn paginate
+  "Split a string up into a vector of text pages"
+  [text]
+  (map #(apply str %) (split-n 250 (convert-to-words text))))
