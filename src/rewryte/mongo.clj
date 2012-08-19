b206644010a6980fe36ca6dce8714c3dc2f78f14
Ron Toland <rontoland@gmail.com>
2012-08-19 12:22:46 -0700

    Update mongo db and collection name to match latest documentation

--- src/rewryte/mongo.clj
+++ src/rewryte/mongo.clj
@@ -1,5 +1,5 @@
 (ns rewryte.mongo
-  (:require [monger.core :as mcore] 
+  (:require [monger.core :as mcore]
             [monger.collection :as mcoll]))
 
 (def mongo-host {:host "127.0.0.1" :port 27017})
@@ -10,13 +10,13 @@
   (let [doc-match {:account_id account-id :document_name doc-name}
         doc-update {:frequencies results-map :results results-string}]
     (mcore/connect! mongo-host)
-    (mcore/set-db! (mcore/get-db "data"))
-    (mcoll/update "docs" doc-match {:$set doc-update})))
+    (mcore/set-db! (mcore/get-db "docs"))
+    (mcoll/update "account" doc-match {:$set doc-update})))
 
 (defn get-document
   "Fetch the given document from mongodb"
   [account-id doc-name]
   (let [doc-match {:account_id account-id :document_name doc-name}]
     (mcore/connect! mongo-host)
-    (mcore/set-db! (mcore/get-db "data"))
-    (mcoll/find-one-as-map "docs" doc-match))) 
+    (mcore/set-db! (mcore/get-db "docs"))
+    (mcoll/find-one-as-map "account" doc-match)))
