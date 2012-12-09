861b6faf586f267223998f83f20daa67d77f6906
Ron Toland <rontoland@gmail.com>
2012-12-09 11:42:31 -0800

    Replace wabbit-mq library with langohr.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject rewryte "0.3.0-SNAPSHOT"
   :description "Application for calculating writing style"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.mefesto/wabbitmq "0.2.1"]
+                 [com.novemberain/langohr "1.0.0-beta10"]
                  [com.novemberain/monger "1.1.2"]
                  [cheshire "4.0.1"]]
   :main rewryte.core
