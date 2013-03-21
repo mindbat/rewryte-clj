f2c319a19f1023ca93a4ebf948df3f86d66358b2
Ron Toland <rontoland@gmail.com>
2013-03-20 19:35:00 -0700

    Add commented versions of consumer functions as goals to implement

--- src/rewryte/consumer.clj
+++ src/rewryte/consumer.clj
@@ -1,4 +1,5 @@
 (ns rewryte.consumer
+  (:use rewryte.process, rewryte.mongo, rewryte.rabbit)
   (:require [clojure.string :as clj-str]))
 
 (defn frequency-consumer [message-body]
@@ -24,6 +25,19 @@
     (save-results account-id doc-id url-name results-full results-standard frequencies max-frequency-full max-frequency-standard paragraphs longest-sentences most-adverbs sentence-length paragraph-length-words paragraph-length-sentences)
     (send-results-published account-id url-name)))
 
+(comment
+  (defn frequency-consumer
+    "Process incoming messages from the frequency queue"
+    [message-body]
+    (-> message-body
+        parse-message
+        (get-document "account")
+        calculate-stats
+        calculate-edits
+        add-url-name
+        (save-document "account")
+        publish-results)))
+
 (defn compare-consumer [message-body]
   (let [document-id message-body
         compare-doc (get-document "compare" document-id)
@@ -35,6 +49,16 @@
         paragraph-length-sentences (avg-paragraph-length-sentences document)]
     (save-compare-results document-id frequencies results sentence-length paragraph-length-words paragraph-length-sentences)))
 
+(comment
+  (defn compare-consumer
+    "Process incoming messages from the compare queue"
+    [message-body]
+    (-> message-body
+        parse-message
+        (get-document "compare")
+        calculate-stats
+        (save-document "compare"))))
+
 (defn paragraph-consumer [message-body]
   (let [edit-id message-body
         edit-doc (get-document "edit" edit-id)
@@ -43,3 +67,14 @@
     (update-paragraph edit-doc)
     (queue-doc "frequency.queue" account-id doc-id)
     (delete-doc "edit" edit-id)))
+
+(comment
+  (defn paragraph-consumer
+    "Process incoming messages from the paragraph queue"
+    [message-body]
+    (-> message-body
+        parse-message
+        (get-document "edit")
+        update-paragraph
+        (publish-to-queue "frequency.queue")
+        (delete-doc "edit"))))
