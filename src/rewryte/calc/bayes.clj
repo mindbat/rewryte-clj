9d8a0f6a82f9f364a1177a712487098d77e30d0b
Ron Toland <rontoland@gmail.com>
2013-06-09 09:49:55 -0700

    Revise return values of bayesian functions to match what is expected at each stage

--- src/rewryte/calc/bayes.clj
+++ src/rewryte/calc/bayes.clj
@@ -1,9 +1,10 @@
-(ns rewryte.calc.bayes)
+(ns rewryte.calc.bayes
+  (:use clojure.set))
 
 (defn extract-features
-  "Given a set of feature functions and labels, calculate the values for the given document"
+  "Given a map of feature functions and labels, calculate the values for the given document"
   [feature-functions document]
-  (map #(hash-map (first %) ((last %) document)) feature-functions))
+  (reduce merge {} (map #(hash-map (first %) ((last %) document)) feature-functions)))
 
 (def initial-train-map {:label-dist {} :feature-value-dist {} :feature-label-dist {} :total-docs 0})
 
@@ -28,19 +29,38 @@
 
 (defn train
   "Given a set of feature functions and pre-classfied documents, calculate:
-   1) The number of documents that have each classification
-   2) The number of documents in each classification that have a given value for a given feature
-   3) The number of all documents that have a given value for a given feature
-   4) The total number of documents"
+   1) The number of documents that have each classification (:label-dist)
+   2) The number of documents in each classification that have a given value for a given feature (:feature-value-dist)
+   3) The number of all documents that have a given value for a given feature (:feature-label-dist)
+   4) The total number of documents (:total-docs)"
   [feature-functions classified-docs]
   (->> classified-docs
        (map #(vector (:label %) (extract-features feature-functions (:text %))))
        (reduce increment-distributions initial-train-map)))
 
+(defn get-max-key
+  "Given a map of keys and numerical values, get the key with the maximum value"
+  [max-map]
+  (last (last (apply sorted-map (flatten (vec (map-invert max-map)))))))
+
+(defn p-feature-category
+  "Given a feature, value and category, find the probability that a document in the category has the given feature-value.
+   We default the probability to 1 instead of 0, so if the feature-value is not found for the category, we ignore it"
+  [[feature value] category trained-map]
+  (get-in trained-map [:feature-label-dist category feature value] 1))
+
+(defn p-category
+  "Given a category, find the probability the given document is in that category"
+  [doc-features category trained-map]
+  (let [total-prob-cat (/ ((trained-map :label-dist) category) (trained-map :total-docs))]
+    (reduce * (map #(p-feature-category % category trained-map) doc-features))))
+
 (defn classify
   "Given a document, a set of feature functions and a map of feature distributions, calculate the most likely category for a document"
   [document feature-functions trained-map]
-  )
+  (let [doc-features (extract-features feature-functions document)
+        categories (keys (trained-map :label-dist))]
+    (get-max-key (zipmap categories (map #(p-category doc-features % trained-map) categories)))))
 
 (defn classifier
   "Given a set of feature functions and classified documents, create a function that can classify new documents"
