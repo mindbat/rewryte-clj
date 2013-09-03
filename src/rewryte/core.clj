28bed09376e9d645f95dd0de632e8ed88a778990
Ron Toland <rontoland@gmail.com>
2013-09-03 11:25:27 -0700

    Define new worker procs in procfile so we can scale all the queue consumers separately.

--- src/rewryte/core.clj
+++ src/rewryte/core.clj
@@ -6,7 +6,11 @@
   (cond
    (= consumer "frequency") (do
                               (connect-to-doc-db!)
-                              (future (start-consumer "frequency.queue" frequency-consumer))
-                              (future (start-consumer "compare.queue" compare-consumer))
-                              (future (start-consumer "paragraph.queue" paragraph-consumer)))
+                              (start-consumer "frequency.queue" frequency-consumer))
+   (= consumer "compare") (do
+                            (connect-to-doc-db!)
+                            (start-consumer "compare.queue" compare-consumer))
+   (= consumer "paragraph") (do
+                              (connect-to-doc-db!)
+                              (start-consumer "paragraph.queue" paragraph-consumer))
    :else (println "No consumer by that name available")))
