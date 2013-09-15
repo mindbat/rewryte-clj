baf0f4e30b5ff2390fdb2f8b29aab875ee255168
Ron Toland <rontoland@gmail.com>
2013-09-15 11:16:34 -0700

    Finish out functions to save and fetch training data from mongo db. Ensure training map saved as strings to be mongo compatible and re-converted back from strings on fetch from mongo

--- src/rewryte/genre.clj
+++ src/rewryte/genre.clj
@@ -5,12 +5,29 @@
                      :sentence-length avg-sentence-length
                      :paragraph-length avg-paragraph-length-words})
 
+(def training-keys [:label-dist :feature-value-dist :feature-label-dist :total-docs])
+
+(defn prep-training-save
+  "Convert training data keys back to strings before saving"
+  [training-map]
+  (apply assoc training-map (flatten (map #(vector % (str (training-map %))) training-keys))))
+
+(defn prep-training-use
+  "Convert training data keys from strings back to maps before use"
+  [training-map]
+  (apply assoc training-map (flatten (map #(vector % (read-string (training-map %))) training-keys))))
+
 (defn get-training-data
-  "Given a classification type, fetch the training data for it from mongo db"
+  "Given a classification type, fetch the training data for it from mongo db. Convert training data from strings to maps before returning"
   [type]
-  (or (first (search-collection {:type type} "training")) (create-document "training" (assoc initial-train-map :account_id 1 :type type))))
+  (prep-training-use
+   (or (first (search-collection {:type type} "training"))
+       (create-document "training" (prep-training-save (assoc initial-train-map :account_id 1 :type type))))))
 
 (defn update-genre-training-data
   "Given a new training document, update the training data in mongodb"
   [training-doc]
-  (update-document "training" (update-trained-map genre-features [training-doc] (get-training-data "genre"))))
+  (->> (get-training-data "genre")
+       (update-trained-map genre-features [training-doc])
+       (prep-training-save)
+       (update-document "training")))
