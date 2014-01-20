4c029f71b1281cc780903eb70adcd7f78764c1b9
Ron Toland <rontoland@gmail.com>
2014-01-20 06:32:52 -0800

    remove extract consumer

--- src/rewryte/core.clj
+++ src/rewryte/core.clj
@@ -1,7 +1,6 @@
 (ns rewryte.core
   (:gen-class :main true)
-  (:require [rewryte.consumer :refer [extract-consumer
-                                      recommend-consumer]]
+  (:require [rewryte.consumer :refer [recommend-consumer]]
             [rewryte.db :refer [connect-to-doc-db!]]
             [rewryte.message :refer [start-consumer]]))
 
@@ -11,7 +10,4 @@
                               (connect-to-doc-db!)
                               (start-consumer "recommend.queue"
                                               recommend-consumer))
-   (= consumer "extract") (do
-                              (connect-to-doc-db!)
-                              (start-consumer "extract.queue" extract-consumer))
    :else (println "No consumer by that name available")))
