86b12dc747c82b80b2b72f443452b6831ac0eaa6
Ron Toland <rontoland@gmail.com>
2014-03-09 10:38:04 -0700

    Use new calc namespace. Fix formatting. Pull report-id from initial message

--- src/rewryte/consumer.clj
+++ src/rewryte/consumer.clj
@@ -1,6 +1,7 @@
 (ns rewryte.consumer
   (:require [clojure.string :as str]
             [rewryte.db :refer [save-recommendations]]
+            [rewryte.calc :refer [calculate-recommendations]]
             [rewryte.message :refer [publish-results]]
             [rewryte.s3 :refer [fetch-s3-document]]
             [rewryte.tika :refer [extract-text]]))
@@ -18,11 +19,13 @@
 (defn realize-map
   "Given a hash-map with unrealized values, realize all values"
   [future-map]
-  (reduce #(assoc %1 (first %2) @(second %2)) future-map (filter #(future? (second %)) future-map)))
+  (reduce #(assoc %1 (first %2) @(second %2))
+          future-map
+          (filter #(future? (second %)) future-map)))
 
 (defn recommend-consumer
   [message-body]
-  (let [[bucket s3-id] (str/split message-body #":")
+  (let [[bucket s3-id report-id] (str/split message-body #":")
         account-id (first (str/split s3-id #"-"))]
     (->> (fetch-s3-document bucket s3-id)
          extract-text
