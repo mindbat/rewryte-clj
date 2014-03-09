905ec10c9a5f0a437aba1301975faccb68815dc5
Ron Toland <rontoland@gmail.com>
2014-03-09 10:38:15 -0700

    Remove connection to mongo db from startup

--- src/rewryte/core.clj
+++ src/rewryte/core.clj
@@ -1,13 +1,10 @@
 (ns rewryte.core
   (:gen-class :main true)
   (:require [rewryte.consumer :refer [recommend-consumer]]
-            [rewryte.db :refer [connect-to-doc-db!]]
             [rewryte.message :refer [start-consumer]]))
 
 (defn -main [consumer]
   (cond
-   (= consumer "recommend") (do
-                              (connect-to-doc-db!)
-                              (start-consumer "recommend.queue"
-                                              recommend-consumer))
+   (= consumer "recommend") (start-consumer "recommend.queue"
+                                            recommend-consumer)
    :else (println "No consumer by that name available")))
