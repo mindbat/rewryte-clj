54127357e14510506b2c9981476e5a05b456754f
Ron Toland <rontoland@gmail.com>
2014-05-04 11:40:44 -0700

    Adjust recommendations to save recommendation-expressions

--- src/rewryte/calc.clj
+++ src/rewryte/calc.clj
@@ -3,22 +3,24 @@
 
 (defn find-offsets
   "Find the indexes at which the given sub string occurs in the larger string"
-  [search-string sub-string]
+  [search-string [id sub-string]]
   (let [sub-length (.length sub-string)]
     (loop [idx 0
            offsets []]
       (let [offset (.indexOf search-string sub-string idx)]
         (if (= offset -1)
           offsets
-          (recur (+ offset sub-length) (conj offsets [offset sub-length])))))))
+          (recur (+ offset sub-length)
+                 (conj offsets [id offset sub-length])))))))
 
 (defn find-cliche-matches
   [cliches text]
-  (let [match-seq (mapcat #(re-seq % text) cliches)]
-    (for [match match-seq]
+  (let [match-seq (mapcat #([(:id %) (re-seq (:expression %) text)])
+                          cliches)]
+    (for [[id match] match-seq]
       (if (coll? match)
-        (first match)
-        match))))
+        [id (first match)]
+        [id match]))))
 
 (defn calculate-recommendations
   [doc-map]
